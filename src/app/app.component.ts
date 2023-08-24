26f79d1f31f1096b7d52caaedef9c8676a0d22b0
blockteamdeveloper <chris+github-external@frontr.co.uk>
2023-08-24 16:21:55 +0100

    introduce with a FormsModule

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,54 +3,34 @@ import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { AppService } from './app.service';
-import {
-  Observable,
-  Subject,
-  debounceTime,
-  distinctUntilChanged,
-  map,
-  switchMap,
-  withLatestFrom,
-} from 'rxjs';
+import { Observable } from 'rxjs';
 import { HttpClientModule } from '@angular/common/http';
 import { Dynos } from './dynos.interfaces';
 
 @Component({
   standalone: true,
-  imports: [RouterModule, CommonModule, HttpClientModule],
+  imports: [RouterModule, CommonModule, FormsModule, HttpClientModule],
   providers: [AppService],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  // added this as simpliest way to stop them clicking search before we get data back
+  // where is ngrx when i need it.
+  loading: boolean;
   dynos$!: Observable<Dynos[]>;
-  private searchTerms$ = new Subject<string>();
+  searchTerm: string;
 
   // For Prod I would call it DynoServicers
-  constructor(private appService: AppService) {}
-
-  search(term: string): void {
-    this.searchTerms$.next(term);
+  constructor(private appService: AppService) {
+    // lets be strick and have things intialiazed in contructor
+    this.searchTerm = '';
+    this.loading = false;
   }
-
-  ngOnInit(): void {
-    // This example shows with Observables
-    this.dynos$ = this.searchTerms$.pipe(
-      // wait 300ms after each keystroke before considering the term
-      debounceTime(300),
-
-      // ignore new term if same as previous term
-      distinctUntilChanged(),
-
-      // switch to new search observable each time the term changes
-      switchMap((term: string) => this.appService.searchDynos(term)),
-      withLatestFrom(this.searchTerms$),
-      map(([data, term]) => {
-        return data.filter((dyno: Dynos) => {
-          return dyno.Name.toLowerCase().includes(term.toLowerCase());
-        });
-      })
-    );
+  onSearch(): void {
+    this.loading = true;
+    this.dynos$ = this.appService.searchDynos(this.searchTerm);
+    this.loading = false;
   }
 }
--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,10 +1,12 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { catchError, tap } from 'rxjs/operators';
+import { catchError, map, tap } from 'rxjs/operators';
+import { Dynos } from './dynos.interfaces';
 
 @Injectable({ providedIn: 'root' })
 export class AppService {
+  // found a dyno api
   private apiUrl = 'https://dinosaur-facts-api.shultzlab.com/dinosaurs';
   httpOptions = {
     headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
@@ -13,7 +15,13 @@ export class AppService {
   constructor(private http: HttpClient) {}
 
   searchDynos(term: string): Observable<any[]> {
-    // If they api would accept a term, I would pass the term in
-    return this.http.get<any[]>(this.apiUrl);
+    //Using the term and some filter with lowercase to make it case insensitive
+    return this.http.get<any[]>(this.apiUrl).pipe(
+      map((data: Dynos[]) => {
+        return data.filter((dyno: Dynos) => {
+          return dyno.Name.toLowerCase().includes(term.toLowerCase());
+        });
+      })
+    );
   }
 }
